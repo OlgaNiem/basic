eba68e755b0d0fdd4feda80aad110593d013cfe5
OlgaNiem <102143760+OlgaNiem@users.noreply.github.com>
2023-04-17 08:47:56 +0300

    update

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from '../../styles/Home.module.css'
+
+const Form = () => {
+  return (
+    <div  className={style.form}>Form</div>
+
+  )
+}
+
+export default Form
